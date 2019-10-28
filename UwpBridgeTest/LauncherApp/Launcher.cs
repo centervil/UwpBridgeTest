3474cb8b880b911adcd9dd354c668a3a85238a8e
ak-nkmr <centervil.en@gmail.com>
2019-10-29 00:43:10 +0900

    LauncherAppの一つ上のディレクトリからの相対パスを引数で受け取り起動するよう変更 #4

--- UwpBridgeTest/LauncherApp/Launcher.cs
+++ UwpBridgeTest/LauncherApp/Launcher.cs
@@ -2,19 +2,19 @@
 using System.IO;
 using System.Diagnostics;
 using System.Reflection;
-class Program
+class Launcher
 {
     static void Main(string[] args)
     {
         try
         {
-
             if (args.Length != 0)
             {
-                string executable = args[2];
+                string relativeExePath = args[2];
                 string path = Assembly.GetExecutingAssembly().CodeBase;
                 string directory = Path.GetDirectoryName(path);
-                Process.Start(directory + "\\" + executable);
+                string upperDirectory = directory.Substring(0, directory.LastIndexOf(@"\"));
+                Process.Start(upperDirectory + "\\" + relativeExePath);
             }
         }
         catch (Exception e)
